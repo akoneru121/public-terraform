dad6e2cabcb10ee8972991720e1e50612d5f3110
Aditya Koneru <koneruaditya121@gmail.com>
2026-01-31 15:26:25 -0600

    updated the output

--- outputs.tf
+++ outputs.tf
@@ -2,46 +2,46 @@
 # Root Module Outputs
 # ==============================================================================
 
-output "cluster_id" {
-  description = "EKS cluster ID"
-  value       = module.eks.cluster_id
-}
+# output "cluster_id" {
+#   description = "EKS cluster ID"
+#   value       = module.eks.cluster_id
+# }
 
-output "cluster_name" {
-  description = "EKS cluster name"
-  value       = module.eks.cluster_name
-}
+# output "cluster_name" {
+#   description = "EKS cluster name"
+#   value       = module.eks.cluster_name
+# }
 
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
-}
+# output "cluster_endpoint" {
+#   description = "Endpoint for EKS control plane"
+#   value       = module.eks.cluster_endpoint
+# }
 
-output "cluster_security_group_id" {
-  description = "Security group ID attached to the EKS cluster"
-  value       = module.eks.cluster_security_group_id
-}
+# output "cluster_security_group_id" {
+#   description = "Security group ID attached to the EKS cluster"
+#   value       = module.eks.cluster_security_group_id
+# }
 
-output "cluster_certificate_authority_data" {
-  description = "Base64 encoded certificate data required to communicate with the cluster"
-  value       = module.eks.cluster_certificate_authority_data
-  sensitive   = true
-}
+# output "cluster_certificate_authority_data" {
+#   description = "Base64 encoded certificate data required to communicate with the cluster"
+#   value       = module.eks.cluster_certificate_authority_data
+#   sensitive   = true
+# }
 
-output "cluster_oidc_issuer_url" {
-  description = "The URL on the EKS cluster OIDC Issuer"
-  value       = module.eks.oidc_provider_url
-}
+# output "cluster_oidc_issuer_url" {
+#   description = "The URL on the EKS cluster OIDC Issuer"
+#   value       = module.eks.oidc_provider_url
+# }
 
-output "node_group_id" {
-  description = "EKS node group ID"
-  value       = module.node_groups.node_group_id
-}
+# output "node_group_id" {
+#   description = "EKS node group ID"
+#   value       = module.node_groups.node_group_id
+# }
 
-output "node_security_group_id" {
-  description = "Security group ID attached to the EKS nodes"
-  value       = module.node_groups.node_security_group_id
-}
+# output "node_security_group_id" {
+#   description = "Security group ID attached to the EKS nodes"
+#   value       = module.node_groups.node_security_group_id
+# }
 
 output "vpc_id" {
   description = "VPC ID"
@@ -57,7 +57,7 @@ output "public_subnets" {
 
 
 
-output "configure_kubectl" {
-  description = "Command to configure kubectl"
-  value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${module.eks.cluster_name}"
-}
+# output "configure_kubectl" {
+#   description = "Command to configure kubectl"
+#   value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${module.eks.cluster_name}"
+# }
