ea9f78dad7542d30caadbd68e934cf0d723eeb0b
Aditya Koneru <koneruaditya121@gmail.com>
2026-02-01 19:07:12 -0600

    format terraform files

--- main.tf
+++ main.tf
@@ -50,40 +50,40 @@ module "vpc" {
 # ==============================================================================
 # EKS Module
 # ==============================================================================
-  module "eks" {
-    source = "./modules/eks"
+module "eks" {
+  source = "./modules/eks"
 
-    project_name = var.project_name
-    vpc_id       = module.vpc.vpc_id
+  project_name = var.project_name
+  vpc_id       = module.vpc.vpc_id
 
-    subnet_ids = module.vpc.public_subnet_ids
+  subnet_ids = module.vpc.public_subnet_ids
 
-    kubernetes_version     = var.kubernetes_version
-    endpoint_public_access = true
-    common_tags            = local.common_tags
+  kubernetes_version     = var.kubernetes_version
+  endpoint_public_access = true
+  common_tags            = local.common_tags
 
-    depends_on = [module.vpc]
- }
+  depends_on = [module.vpc]
+}
 
 # ==============================================================================
 # Node Groups Module
 # ==============================================================================
- module "node_groups" {
-   source = "./modules/node_groups"
+module "node_groups" {
+  source = "./modules/node_groups"
 
-   project_name              = var.project_name
-   vpc_id                    = module.vpc.vpc_id
-   cluster_name              = module.eks.cluster_name
-   cluster_security_group_id = module.eks.cluster_security_group_id
+  project_name              = var.project_name
+  vpc_id                    = module.vpc.vpc_id
+  cluster_name              = module.eks.cluster_name
+  cluster_security_group_id = module.eks.cluster_security_group_id
 
-   subnet_ids = module.vpc.public_subnet_ids
+  subnet_ids = module.vpc.public_subnet_ids
 
-   instance_types = var.instance_types
-   capacity_type  = var.capacity_type
-   desired_size   = var.node_group_desired_size
-   min_size       = var.node_group_min_size
-   max_size       = var.node_group_max_size
-   common_tags    = local.common_tags
+  instance_types = var.instance_types
+  capacity_type  = var.capacity_type
+  desired_size   = var.node_group_desired_size
+  min_size       = var.node_group_min_size
+  max_size       = var.node_group_max_size
+  common_tags    = local.common_tags
 
-   depends_on = [module.eks]
- }
+  depends_on = [module.eks]
+}
